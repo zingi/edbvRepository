e0f71cf220e97e6b823b510d481eadb5a7e24095
DESKTOP-2QPUJR4\MarkusP <pmarkus@gmx.net>
2017-01-08 13:13:00 +0100

    Made comments and restructured code

--- boundcheck.m
+++ boundcheck.m
@@ -1,3 +1,4 @@
+%Check if imagepoint is inside specified dimensions
 function Out = boundcheck(pointX,pointY,dimX,dimY)
     if pointX < 1 || pointY < 1 || pointX > dimX || pointY > dimY
         Out = false;
